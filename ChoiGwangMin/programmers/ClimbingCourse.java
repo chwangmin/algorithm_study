a9579a1f2b8fa4dfe12bb0a9ecfbe026196f1f45
chwangmin <44563065+chwangmin@users.noreply.github.com>
2024-03-07 10:40:08 +0900

    [refactor] ClimbingCourse.java

--- ChoiGwangMin/programmers/ClimbingCourse.java
+++ ChoiGwangMin/programmers/ClimbingCourse.java
@@ -8,71 +8,64 @@ class Solution {
             endNode = new ArrayList<>();
         }
     }
-    
-    class EndWeight implements Comparable<EndWeight>{
+
+    class EndWeight {
         int e, w;
         EndWeight(int e, int w){
             this.e = e;
             this.w = w;
         }
-        @Override
-        public int compareTo(EndWeight o){
-            return Integer.compare(o.w, this.w);
-        }
     }
-    
+
     public int[] solution(int n, int[][] paths, int[] gates, int[] summits) {
         int[] answer = new int[2];
         answer[1] = 1_234_567_890;
         Node[] nodes = new Node[n+1];
-        
+
         for (int i = 1 ; i < n+1; i++){
             nodes[i] = new Node();
         }
-        
+
         for (int i = 0 ; i < paths.length; i++){
             nodes[paths[i][0]].endNode.add(new EndWeight(paths[i][1],paths[i][2]));
             nodes[paths[i][1]].endNode.add(new EndWeight(paths[i][0],paths[i][2]));
         }
-             
+
         for(int i = 0 ; i < gates.length; i++){
             int[] visited = new int[n+1];
             Arrays.fill(visited, Integer.MAX_VALUE);
-            PriorityQueue<EndWeight> pq = new PriorityQueue<>();
-            pq.add(new EndWeight(gates[i],0));
+            Deque<EndWeight> q = new ArrayDeque<>();
+            q.add(new EndWeight(gates[i],0));
             visited[gates[i]] = 0;
             boolean flag = false;
-            while(!pq.isEmpty()){
-                EndWeight ew = pq.poll();
+            while(!q.isEmpty()){
+                EndWeight ew = q.poll();
                 int current = ew.e;
                 for (int k = 0 ; k < nodes[current].endNode.size(); k++){
                     int tmpCurrentNode = nodes[current].endNode.get(k).e;
                     int tmpCurrentWeight = nodes[current].endNode.get(k).w;
-                    int tmpWeight = Math.max(ew.w, tmpCurrentWeight);
-                    if(visited[tmpCurrentNode] <= tmpWeight)
-                        continue;
-                    if(tmpCurrentWeight > answer[1])
-                        continue;
+                    int currentMaxWeight = Math.max(ew.w, tmpCurrentWeight);
+                    if(visited[tmpCurrentNode] <= currentMaxWeight) continue;
+                    if(currentMaxWeight > answer[1]) continue;
                     for (int j = 0 ; j < summits.length; j++){
                         if (summits[j] == tmpCurrentNode){
-                            int currentMax = Math.max(ew.w,tmpCurrentWeight);
-                            if (answer[1] > currentMax){
+                            if (answer[1] > currentMaxWeight){
                                 answer[0] = tmpCurrentNode;
-                                answer[1] = currentMax;
                             }
-                            else if (answer[1] == currentMax){
+                            else if (answer[1] == currentMaxWeight){
                                 answer[0] = Math.min(answer[0],tmpCurrentNode);
                             }
+                            answer[1] = currentMaxWeight;
                             flag = true;
                             break;
                         }
                     }
+                    visited[tmpCurrentNode] = currentMaxWeight;
                     if (flag){
                         flag = false;
                         continue;
                     }
-                    visited[tmpCurrentNode] = tmpWeight;
-                    pq.add(new EndWeight(tmpCurrentNode,tmpWeight));
+                    q.add(new EndWeight(tmpCurrentNode,currentMaxWeight));
                 }
             }
         }
